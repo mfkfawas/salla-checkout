40482af784b452cd26aa214a7ad3149dba7f289a
mfkfawas <mohammed.fawas@mobcoder.com>
2024-03-13 22:21:36 +0300

    build generated

--- stencil.config.ts
+++ stencil.config.ts
@@ -4,6 +4,8 @@ import tailwind, { tailwindHMR, setPluginConfigurationDefaults } from 'stencil-t
 import tailwindcss from 'tailwindcss';
 import tailwindConf from './tailwind.config';
 import autoprefixer from 'autoprefixer';
+import { reactOutputTarget } from '@stencil/react-output-target';
+import { vueOutputTarget } from '@stencil/vue-output-target';
 
 setPluginConfigurationDefaults({
   tailwindConf,
@@ -23,9 +25,17 @@ export const config: Config = {
     {
       type: 'www',
       // comment the following line to disable service workers in production
-      serviceWorker: null,
+      // serviceWorker: null,
       baseUrl: 'https://myapp.local/',
     },
+    // reactOutputTarget({
+    //   componentCorePackage: 'checkout',
+    //   proxiesFile: './src/components/react/index.ts',
+    // }),
+    // vueOutputTarget({
+    //   componentCorePackage: 'checkout',
+    //   proxiesFile: './src/components/vue/index.ts',
+    // }),
   ],
   plugins: [sass(), tailwind(), tailwindHMR()],
 };
